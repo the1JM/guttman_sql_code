0493f7cc6d54de2ca5ce75c90fbec792b96b5b2e
Jorge Morales <jm@Jorges-MacBook-Pro.local>
2024-01-27 12:02:32 -0500

    20240127 creating roles

---
+++ db_installs/my_roles.sql
@@ -0,0 +1,16 @@
+-- DROP ROLES
+DROP ROLE
+IF EXISTS 'read_only_classicmodels_db'@'%';
+
+-- CHECK TO SEE GONE
+SELECT user, show_db_priv, account_locked
+FROM mysql.user;
+
+-- CREATE ROLES
+CREATE ROLE
+IF NOT EXISTS
+'read_only_classicmodels_db', 'admin_user',
+'read_only_employees_db', 'app_user' ;
+
+SELECT user, show_db_priv, account_locked
+FROM mysql.user;
